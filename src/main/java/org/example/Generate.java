e5989abbcbb42caec6c35cd7e36d30ef0b21b4a4
irmazhinkina <irma.zhinkina@jetbrains.com>
2024-05-30 09:13:51 +0000

    Some new files

--- src/main/java/org/example/Generate.java
+++ src/main/java/org/example/Generate.java
@@ -3,13 +3,13 @@ package org.example;
 class Generate {
     int xxx = 5;
 
-    public Generate() {
+    public Generate(int xxx) {
         this.xxx = xxx;
     }
 
     public static void main(String[] args) {
         Generate test = new Generate();
         System.out.println(test.xxx);//test
-        System.out.println("test");//test
+        System.out.println("test");
     }
 }
