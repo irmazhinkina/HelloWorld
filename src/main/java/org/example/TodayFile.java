1262ac0cfb94237a8159ca7bcebbbbd1943dce9d
irmazhinkina <irma.zhinkina@jetbrains.com>
2024-06-19 13:07:22 +0000

    Added TodayFile

---
+++ src/main/java/org/example/TodayFile.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class TodayFile {
+    int yyy = 5;
+
+    public static void main(String[] args) {
+        Generate test = new Generate();
+        System.out.println(test.yyy);//test
+        System.out.println("test");
+    }
+
+}
