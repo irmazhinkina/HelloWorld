af087756845fc17fcfef6c888468f2df0492e7df
Irma.Zhinkina <irmamalysheva@gmail.com>
2024-06-28 15:41:27 +0200

    123

---
+++ src/main/java/org/example/Tests.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Tests {
+    static class Generate {
+        int xxx = 5;
+
+        public static void main(String[] args) {
+            Generate test = new Generate();
+            System.out.println(test.xxx);//test
+            System.out.println("test");
+
+        }
+    }
+}
