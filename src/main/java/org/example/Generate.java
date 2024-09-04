4f713471cb18ae2d4c74105dd145428629164f24
irmazhinkina <irma.zhinkina@jetbrains.com>
2024-09-04 21:07:32 +0000

    Changes

--- src/main/java/org/example/Generate.java
+++ src/main/java/org/example/Generate.java
@@ -3,11 +3,17 @@ package org.example;
 class Generate {
     int xxx = 5;
 
+    public Generate(int xxx) {
+        this.xxx = xxx;
+    }
+
+    public Generate() {
+
+    }
+
     public static void main(String[] args) {
         Generate test = new Generate();
         System.out.println(test.xxx);//test
         System.out.println("test");
-
     }
 }
-
